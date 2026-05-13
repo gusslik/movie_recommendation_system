9c5b4530c7b613dfd9ba91c181571dc7ee9280e7
gusslik <grigory.bocharsky@gmail.com>
2026-05-13 22:46:30 +0300

    Сделано форматирование данных в справочники в формате 3нф

--- Scripts/backend/utils.py
+++ Scripts/backend/utils.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import csv
 from os import path, getcwd
 
 DATA_DIR_PATH = path.join(getcwd(), "Data")
@@ -22,4 +21,52 @@ def load_data_csv(filepath: str) -> dict:
 
     return raw
 
-print(load_data_csv(DATA_DIR_PATH))
+# Форматирует сырые данные из load_data_csv и формирует таблицы в 3нф
+def convert_to_3nf(raw: dict) -> dict:
+    movies_raw = raw['movies']
+    ratings_raw = raw['ratings']
+    tags_raw = raw['tags']
+
+    unique_users = set()
+    for value in ratings_raw["userId"]:
+        unique_users.add(value)
+
+    users = pd.DataFrame({
+        "userId": sorted(unique_users)
+    })
+
+    movies = movies_raw[["movieId", "title"]]
+
+    genres_split = set()
+    for value in movies_raw["genres"]:
+        genres_split.update(value.split('|'))
+
+    genres = pd.DataFrame({
+        "genreId": range(1, len(genres_split) + 1),
+        "genres": sorted(genres_split)
+    })
+
+    genre_map = dict(zip(genres["genres"], genres["genreId"]))
+
+    movie_genre_rows = []
+
+    for row in movies_raw.itertuples(index=False):
+        for genre_name in row.genres.split('|'):
+            movie_genre_rows.append({
+                "movieId": row.movieId,
+                "genreId": genre_map[genre_name]
+            })
+
+    movie_genre = pd.DataFrame(movie_genre_rows)
+
+    ratings: pd.DataFrame = ratings_raw
+    ratings["ratingId"] = range(1, len(ratings_raw) + 1)
+
+    tags: pd.DataFrame = tags_raw
+    tags["tagId"] = range(1, len(tags_raw) + 1)
+
+    print(movie_genre) 
+
+    
+    
+convert_to_3nf(load_data_csv(DATA_DIR_PATH))
